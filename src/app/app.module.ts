5df48e1dc6bdfc08bb68317d1bd176e5f31fbf6e
Faizal <faizal@geatfixtechnologies.com>
2021-11-02 12:47:10 +0530

    2nd  commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,58 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NO_ERRORS_SCHEMA,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DepartmentComponent } from './department/department.component';
+import { ShowDepComponent } from './department/show-dep/show-dep.component';
+import { AddEditDepComponent } from './department/add-edit-dep/add-edit-dep.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { EditAddComponent } from './employee/edit-add/edit-add.component';
+import { ShowEmpComponent} from './employee/show-emp/show-emp.component';
+import { SharedService }  from './shared.service';
+
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentComponent,
+    ShowDepComponent,
+    AddEditDepComponent,
+    EmployeeComponent,
+    EditAddComponent,
+    NavigationComponent,
+    ShowEmpComponent,
+    
+   
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule
+   
+    
+    
+
+
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [SharedService],
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA,
+    NO_ERRORS_SCHEMA
+  ]
 })
 export class AppModule { }
